2f38772e819b30614dbca8315b3aad94281d4e8d
Cao Meng <1596105877@qq.com>
2020-06-30 13:23:21 +0800

    del

--- 手写数字识别/GitHub/1.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from numpy import *
-
-r0=4
-c0=5
-state = zeros((r0, c0,10))
-print(state)
--- 手写数字识别/GitHub/gParam.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-TOP_PATH = 'test/'
-LAB_PATH = 'label/label.txt'
-C_SIZE = 5
-F_NUM = 12
-P_SIZE = 2
-FILE_TYPE = '.png'
-MAX_ITER_NUM = 10
